98558bb2da20ef83cdf8d492a59eb5e29d6a785c
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2018-12-18 22:49:29 +0100

    chore(setup): update package.json - move babel to dependencies - update node version - install helmet [Finishes #162735861]

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import routes from './routes';
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 if (process.env.NODE_ENV === 'development') app.use(logger('dev'));
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
